216b0cefd1a344c7fc9c5efa657038082166b48b
lenovo <lenovo@Omm-984>
2025-06-27 17:26:08 +0530

    new flipcart

--- DDT_framework/src/test/java/drivefactory/flipkart.java
+++ DDT_framework/src/test/java/drivefactory/flipkart.java
@@ -1,9 +1,38 @@
 package drivefactory;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class flipkart {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		WebElement searchbox= driver.findElement(By.name("q"));
+		searchbox.sendKeys("samsung a54");
+		searchbox.submit();
+		String mainpage =driver.getWindowHandle();
+		System.out.println("Main page"+ mainpage);
+		driver.findElement(By.xpath("//div[normalize-space()=\"Samsung Galaxy A54 5G (Awesome Graphite, 256 GB)\"]")).click();
+        Set<String>list=driver.getWindowHandles();
+        for (String string : list) {
+	    System.out.println(string);
+	    String title=driver.switchTo().window(string).getTitle();
+	   if( title.equals(list)) {
+		driver.close();
+	     }
+       }
+	   Thread.sleep(2000);
+	
+	    driver.findElement(By.xpath("//button[@class=\"QqFHMw vslbG+ In9uk2\"]")).click();
+		
+		
+		
 
 	}
 
